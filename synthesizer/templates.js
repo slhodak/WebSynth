3d3b7619b3808db7320e2fbd3bf8c3b9796056fa
Sam Hodak <slhodak@gmail.com>
2019-07-07 13:24:37 -0700

    Remove unnecessary string concat from existing templates

--- synthesizer/templates.js
+++ synthesizer/templates.js
@@ -1,9 +1,9 @@
 const Template = {
   selector(id, name, title, options, optionTitles) {
     let template = 
-      `<div class="selector">` +
-      `<label for="${name}">${title}: </label>` +
-      `<select class="${name}" data-id=${id}>`;
+      `<div class="selector">
+      <label for="${name}">${title}: </label>
+      <select class="${name}" data-id=${id}>`;
     for (let i = 0; i < options.length; i++) {
       template += `<option name="${options[i]}" value="${options[i]}">${optionTitles ? optionTitles[i] : options[i]}</option>`;
     }
@@ -12,10 +12,10 @@ const Template = {
   },
   slider(id, name, title, min, max, value, step) {
     return (
-      `<div class="slider">` +
-      `<label for="${name}">${title}: </label>` +
-      `<input class="${name}" type="range" min="${min}" max="${max}" value="${value}" step="${step}">` +
-      `</div>`
+      `<div class="slider">
+      <label for="${name}">${title}: </label>
+      <input class="${name}" type="range" min="${min}" max="${max}" value="${value}" step="${step}">
+      </div>`
     );
   }
 }
