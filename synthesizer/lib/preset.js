241f137d77353780c15085f4be975301829d3530
Sam Hodak <slhodak@gmail.com>
2019-07-09 15:06:45 -0700

    Add preset-save: Oscillators

--- synthesizer/lib/preset.js
+++ synthesizer/lib/preset.js
@@ -17,7 +17,15 @@ const Preset = {
     synthData.synthesizer.settings.globals.porta = synthesizer.globals.porta;
     synthData.synthesizer.settings.globals.attack = synthesizer.globals.attack;
     synthData.synthesizer.settings.globals.release = synthesizer.globals.release;
-
+    synthesizer.oscillators.forEach((osc, index) => {
+      synthData.synthesizer.oscillators[index] = {
+        id: osc.id,
+        semitoneOffset: osc.semitoneOffset,
+        fineDetune: osc.fineDetune,
+        volume: osc.volume,
+        type: osc.type
+      }
+    });
     console.log(synthData);
   },
   load(synthData) {
