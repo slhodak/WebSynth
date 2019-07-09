dabc7fbc4a5a684874f1265e40d5eba57a6948b9
Sam Hodak <slhodak@gmail.com>
2019-07-09 00:53:28 -0700

    consolidate midi keyboard code

--- synthesizer/midi.js
+++ synthesizer/midi.js
@@ -1,23 +1,34 @@
-let midiKeyboard = {};
-window.navigator.requestMIDIAccess()
-  .then(midiAccess => {
-    extendKeyboard(midiAccess);
-  })
-  .catch(error => {
-    console.log(error);
-  });
-
-
-function extendKeyboard(midiAccess) {
-  midiKeyboard.midiAccess = midiAccess;
-  midiKeyboard.midiAccess.onstatechange = (connection) => { 
-    midiKeyboard.connection = connection;
-    midiKeyboard.connection.port.onmidimessage = (message) => {
-      if (message.data[0] === 144) {
-        synthesizer.playNote(message);
-      } else if (message.data[0] === 128) {
-        synthesizer.endNote(message);
+let MIDIKeyboard = {
+  connect() {
+    window.navigator.requestMIDIAccess()
+      .then(midiAccess => {
+        console.log(midiAccess);
+        MIDIKeyboard.create(midiAccess);
+        console.log(midiAccess);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  },
+  create(midiAccess) {
+    MIDIKeyboard.midiAccess = midiAccess;
+    MIDIKeyboard.midiAccess.onstatechange = (connection) => {
+      MIDIKeyboard.connection = connection;
+      MIDIKeyboard.connection.port.onmidimessage = (message) => {
+        if (synthesizer) {
+          if (message.data[0] === 144) {
+            console.log('byte length ' + message.data.buffer.byteLength);
+            console.log('byte offset ' + message.data.byteOffset);
+            console.log(message);
+            synthesizer.playNote(message);
+          } else if (message.data[0] === 128) {
+            synthesizer.endNote(message);
+          }
+        }
+        MIDIKeyboard.midiAccess.onstatechange = null;
       }
-    }
-  };
-}
+    };
+  }
+};
+
+MIDIKeyboard.connect();
