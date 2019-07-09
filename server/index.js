673f5c78f43dae3506abeba2b559374a84ef6a48
Sam Hodak <slhodak@gmail.com>
2019-07-09 15:23:24 -0700

    Create server route for preset json

--- server/index.js
+++ server/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const path = require('path');
+const presets = require('./presets');
 
 const port = 3000;
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../dist')));
 app.use('/synthesizer', express.static(path.resolve(__dirname, '../synthesizer')));
+app.use(express.json());
+
+app.post('/preset', (req, res) => {
+  console.log(req.body);
+  res.status(200).send('heard ya!');
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
