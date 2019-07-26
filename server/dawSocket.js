574d39ff5e8fc8d58a01ddaeb0e16195b5d6063a
Sam Hodak <slhodak@gmail.com>
2019-07-26 16:24:28 -0700

    Create DawSocket class

--- server/dawSocket.js
+++ server/dawSocket.js
@@ -1,14 +1,22 @@
 const ws = require('ws');
 
+const DawSocket = (socket) => {
+  this.socket = socket;
+  this.socket.on('message', (message) => {
+    console.log(message);
+  });
+  this.sendNameUpdate = (oldName, newName) => {
+    this.socket.send({ rename: [oldName, newName ]});
+  };
+};
+
 const wss = new ws.Server({
   port: 3001
 }, () => {
   console.log(`WebSocket Server connected on port ${wss.port}`);
-  wss.on('connection', (connection) => {
-    connection.on('message', (message) => {
-      console.log(message);
-    });
+  wss.on('connection', (socket) => {
+    wss.dawSocket = new DawSocket(socket);
   });
 });
 
-module.exports = wss;
+module.exports = DawSocket;
